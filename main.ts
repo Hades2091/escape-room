b3be233726e3986dca079e80bc010763950e09d8
Hades2091 <16flirskim@rougemontschool.co.uk>
2026-01-22 20:22:28 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,9 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     music.stopAllSounds()
     music.play(music.createSong(hex`0078000408020109010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8001800000001000100080009000100100011000100180019000100`), music.PlaybackMode.UntilDone)
 })
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    music.stopAllSounds()
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . . f f f f f f . . . . . 
@@ -36,7 +39,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     music.play(music.createSong(hex`0078000408020109010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8001800000001000100080009000100100011000100180019000100`), music.PlaybackMode.UntilDone)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Door, function (sprite, otherSprite) {
-	
+    tiles.setCurrentTilemap(tilemap`level2`)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
@@ -64,15 +67,15 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . f 4 4 f f 4 4 f . . . . 
-        . . f f f 2 2 f f 2 2 f . . . . 
-        . . f 4 4 4 4 f f f f f f f . . 
-        . . f 4 4 4 4 2 2 2 2 d d f . . 
-        . . f f 4 4 4 4 2 2 2 2 2 f . . 
-        . . . f 4 4 4 4 2 2 2 2 2 f . . 
-        . . . f f 4 4 4 4 2 2 2 f f . . 
-        . . . . f f f 4 4 2 2 f f . . . 
+        . . . . f 2 2 f f 2 2 f f f . . 
+        . . f f f f f f f 4 4 4 4 f . . 
+        . . f d d 2 2 2 2 4 4 4 4 f . . 
+        . . f 2 2 2 2 2 4 4 4 4 f f . . 
+        . . f 2 2 2 2 2 4 4 4 4 f . . . 
+        . . f f 2 2 2 4 4 4 4 f f . . . 
+        . . . f f 2 2 4 4 f f f . . . . 
         . . . . f f d d d d f f . . . . 
-        . . . . f 4 4 4 d d d f . . . . 
+        . . . . f d d d 4 4 4 f . . . . 
         . . . f f 4 4 4 4 4 4 f f . . . 
         . . . f 4 4 4 4 4 4 4 4 f . . . 
         . . . f 4 4 4 4 4 4 4 4 f . . . 
@@ -86,22 +89,22 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let mySprite: Sprite = null
 let mySprite2 = sprites.create(img`
-    c c c 3 3 f f f f f f f 3 c c c 
-    f f f f 3 f 4 e e 4 e f 3 f f f 
-    f f f c f f 4 e e 4 e f 3 f f f 
-    c c c c f e 4 e 4 4 e f 3 3 c c 
-    3 3 3 3 f e 4 e 4 e e f 3 3 3 3 
-    3 c f f f e 4 e 4 e 4 4 f f f c 
-    3 3 c c f 4 4 e 4 e 4 4 f c c c 
-    3 3 3 f f 4 4 4 4 e 4 e f 3 3 3 
-    f f c 3 f 4 4 e 4 e 5 f f c f f 
-    c c c 3 f e 4 e 4 e 4 f f c c c 
-    3 3 3 f f e 4 e 4 e 4 f 3 3 3 3 
-    3 f f f 4 e 4 e 4 4 4 f c f f 3 
-    3 3 3 f 4 e 4 e 4 4 4 f 3 3 3 3 
-    d d 3 f d d d d d d f f d d d d 
-    c b d d d b d d d d b 3 3 b c d 
-    c c b b d d d b b 3 3 3 b c c d 
+    . . . . . f f f f f f f . . . . 
+    . . . . . f 4 e e 4 e f . . . . 
+    . . . . f f 4 e e 4 e f . . . . 
+    . . . . f e 4 e 4 4 e f . . . . 
+    . . . . f e 4 e 4 e e f . . . . 
+    . . . f f e 4 e 4 e 4 4 f f . . 
+    . . . . f 4 4 e 4 e 4 4 f . . . 
+    . . . f f 4 4 4 4 e 4 e f . . . 
+    . . . . f 4 4 e 4 e 5 f f . . . 
+    . . . . f e 4 e 4 e 4 f f . . . 
+    . . . f f e 4 e 4 e 4 f . . . . 
+    . . f f 4 e 4 e 4 4 4 f . . . . 
+    . . . f 4 e 4 e 4 4 4 f . . . . 
+    . . . f . . . . . . f f . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Door)
 mySprite2.setPosition(120, 8)
 story.setSoundEnabled(true)
