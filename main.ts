bc40bcc4d305cdd0594bc8c43b96bc53a86ee5cb
Hades2091 <16flirskim@rougemontschool.co.uk>
2026-02-02 18:09:36 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,28 +14,28 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     music.play(music.createSong(hex`0078000408020109010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8001800000001000100080009000100100011000100180019000100`), music.PlaybackMode.UntilDone)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
-    story.startCutscene(function () {
-        color.startFade(color.originalPalette, color.Black)
-        color.startFade(color.Black, color.originalPalette)
-        story.cancelAllCutscenes()
-    })
-    tiles.setCurrentTilemap(tilemap`level2`)
-    mySprite.setPosition(120, 30)
-    mySprite.setVelocity(0, 0)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile34`, function (sprite, location) {
     if (Puzzle1A_1 == 6) {
         story.startCutscene(function () {
             color.startFade(color.originalPalette, color.Black)
             color.startFade(color.Black, color.originalPalette)
             story.cancelAllCutscenes()
         })
-        tiles.setCurrentTilemap(tilemap`level`)
+        tiles.setCurrentTilemap(tilemap`level2`)
         mySprite.setPosition(120, 30)
         mySprite.setVelocity(0, 0)
     } else {
         mySprite.y += -5
     }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile34`, function (sprite, location) {
+    story.startCutscene(function () {
+        color.startFade(color.originalPalette, color.Black)
+        color.startFade(color.Black, color.originalPalette)
+        story.cancelAllCutscenes()
+    })
+    tiles.setCurrentTilemap(tilemap`level`)
+    mySprite.setPosition(120, 30)
+    mySprite.setVelocity(0, 0)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
     if (controller.B.isPressed()) {
@@ -133,7 +133,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, 
         game.showLongText("You find a key shard. You need 6", DialogLayout.Bottom)
     }
     Puzzle1A_1 += 1
-    mySprite.x += 5
+    mySprite.x += -5
 })
 let mySprite: Sprite = null
 let Puzzle1A_1 = 0
