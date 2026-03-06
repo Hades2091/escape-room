8ae0778e368b97b6f38d3484ca51ca2a96b4f707
Hades2091 <16flirskim@rougemontschool.co.uk>
2026-03-06 17:22:30 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -41,6 +41,120 @@ namespace myImages {
 . . . . f 2 2 f f 2 2 f f f . . 
 . . . . f 4 4 f f 4 4 f . . . . 
 `;
+            case "image2":
+            case "Character0":return img`
+. . . . . f f f f f f . . . . . 
+. . . f f f 4 4 4 4 f f f . . . 
+. . . f 4 4 4 d 4 4 4 4 f . . . 
+. . . f 4 4 4 d d 4 4 4 f . . . 
+. . . f 4 d f d d f d 4 f . . . 
+. . . f f d d d d d d f f . . . 
+. . . . f 4 4 2 2 4 4 f . . . . 
+. . . . f f 4 4 4 4 f f . . . . 
+. . . f f e e e f f f f . . . . 
+. . f f 2 e 2 4 4 4 5 f f . . . 
+. . f 2 2 e 2 e 4 4 4 4 f . . . 
+. . f d d e 2 e 4 4 4 4 f f . . 
+. . f d d 2 2 2 2 4 4 4 4 f . . 
+. . f f f f f f f 4 4 4 4 f . . 
+. . . . f 2 2 f f 2 2 f f f . . 
+. . . . f 4 4 f f 4 4 f . . . . 
+`;
+            case "image3":
+            case "myImage0":return img`
+f f d d d d d d d d d d d f . . 
+. f d 1 1 1 1 f f 1 1 1 d f f . 
+. f d 1 1 1 1 f f 1 1 1 d d f . 
+. f d 1 1 1 f 1 1 f 1 1 1 d f f 
+f f d d 1 1 f 1 1 f 1 1 1 d f f 
+f f d d 1 1 f 1 1 f 1 1 1 d f f 
+. f d d 1 f 1 1 1 1 f 1 1 d f . 
+. f d d 1 f 1 1 1 1 f 1 1 d f . 
+. f d 1 1 f 1 1 1 1 f 1 1 d f . 
+f f d 1 f 1 1 1 1 1 1 f 1 d f . 
+f d d 1 f 1 1 1 1 1 1 f 1 d f f 
+. f d 1 f 1 1 1 1 1 1 f 1 d f f 
+. f d 1 f f f f f f f f d d f . 
+. f d d 1 1 1 f f 1 1 1 d f . . 
+. f f d 1 1 1 f f 1 1 1 d f f . 
+f f d d 1 1 1 1 1 1 1 1 d d f f 
+`;
+            case "image7":
+            case "myImage4":return img`
+f f d d d d d d d d d d d f . . 
+. f d 1 1 1 1 1 1 1 1 1 d f f . 
+. f d 1 1 1 1 1 f 1 1 1 d d f . 
+. f d 1 1 1 1 f 1 f 1 1 1 d f f 
+f f d f 1 1 f 1 1 f 1 1 1 d f f 
+f f d f f 1 f 1 1 1 f 1 1 d f f 
+. f d f f f f 1 1 1 f 1 1 d f . 
+. f d 1 1 f f 1 1 f f 1 1 d f . 
+. f d 1 1 1 f 1 1 f 1 1 1 d f . 
+f f d 1 1 1 f 1 1 f 1 1 f d f . 
+f d d 1 1 f 1 1 1 f f f f d f f 
+. f d 1 1 f f 1 1 f f f 1 d f f 
+. f d 1 1 1 f 1 1 f 1 1 d d f . 
+. f d d 1 1 f 1 1 f 1 1 d f . . 
+. f f d 1 f f 1 1 f 1 1 d f f . 
+f f d d 1 f 1 1 1 f f 1 d d f f 
+`;
+            case "image6":
+            case "myImage3":return img`
+f f d d d f f d d d d d d f . . 
+. f d f f 1 f f f f 1 1 d f f . 
+. f d f 1 1 1 1 1 1 f f d d f . 
+. f d f 1 1 1 1 1 1 1 1 f d f f 
+f f d 1 f f f 1 1 1 f f f d f f 
+f f d 1 1 1 1 f f f 1 1 1 d f f 
+. f d 1 f 1 1 1 1 1 1 f 1 d f . 
+. f d 1 f 1 1 1 1 1 1 f 1 d f . 
+. f d 1 1 1 f 1 1 f 1 1 1 d f . 
+f f d 1 1 1 f 1 1 1 1 1 f d f . 
+f d d 1 1 1 1 1 1 1 1 1 f d f f 
+. f d 1 1 1 1 1 1 1 1 1 1 d f f 
+. f d 1 f 1 1 f 1 1 f 1 d d f . 
+. f d d f 1 1 1 1 1 f 1 d f . . 
+. f f d 1 1 1 1 f 1 1 1 d f f . 
+f f d d 1 1 f 1 f 1 1 1 d d f f 
+`;
+            case "image5":
+            case "myImage2":return img`
+f f d d d d d d d d d d d f . . 
+. f d 1 1 1 1 f f 1 1 1 d f f . 
+. f d 1 1 f f 1 1 f f 1 d d f . 
+. f d 1 f 1 1 1 1 1 f f 1 d f f 
+f f d 1 f 1 f f f 1 1 f 1 d f f 
+f f d 1 1 f 1 1 1 f 1 f 1 d f f 
+. f d 1 1 1 1 1 1 f 1 f 1 d f . 
+. f d 1 1 1 1 1 1 f 1 f 1 d f . 
+. f d 1 1 1 1 f f f 1 f 1 d f . 
+f f d 1 1 f f 1 1 1 1 f 1 d f . 
+f d d 1 1 f 1 1 1 f f f 1 d f f 
+. f d 1 1 1 f f f 1 1 1 1 d f f 
+. f d 1 1 1 1 f f 1 1 1 d d f . 
+. f d d 1 1 1 f f 1 1 1 d f . . 
+. f f d 1 1 1 f f 1 1 1 d f f . 
+f f d d 1 1 1 f f 1 1 1 d d f f 
+`;
+            case "image4":
+            case "myImage1":return img`
+f f d d d d d d d d d d d f . . 
+. f d 1 1 1 f f f 1 1 1 d f f . 
+. f d 1 1 f f 1 f f 1 1 d d f . 
+. f d 1 1 f 1 1 1 f 1 1 1 d f f 
+f f d d 1 f f 1 f f 1 1 1 d f f 
+f f d 1 1 1 f f f 1 1 1 1 d f f 
+. f d 1 1 1 1 f 1 1 1 1 1 d f . 
+. f d 1 1 1 1 f f 1 1 1 1 d f . 
+. f d 1 1 1 1 f 1 f 1 1 1 d f . 
+f f d 1 1 1 f 1 1 1 1 1 1 d f . 
+f d d 1 1 f f f 1 1 1 1 1 d f f 
+. f d 1 f 1 1 f 1 1 1 1 1 d f f 
+. f d 1 1 1 1 f f 1 1 1 d d f . 
+. f d d 1 1 1 1 f f 1 1 d f . . 
+. f f d 1 1 1 f f 1 1 1 d f f . 
+f f d d 1 1 1 f 1 1 1 1 d d f f 
+`;
         }
         return null;
     })
@@ -53,6 +167,13 @@ namespace myImages {
     })
 
     helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
         switch(helpers.stringTrim(name)) {
 
         }
